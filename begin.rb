c97aae6c35eb2c32ace1940c88bf5129f17882c9
pavel <pavel.roznovskiy@faceit.com.ua>
2011-05-03 10:06:04 +0300

    fixed

--- begin.rb
+++ begin.rb
@@ -5,6 +5,7 @@ require 'uri'
 require 'rubygems'
 require 'spreadsheet'
 
+
 url = URI.parse('http://www.microtron.zp.ua')
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.get('/price/mt1.zip')
@@ -25,24 +26,38 @@ Zip::ZipFile::open("1.zip") {
 FileUtils.remove_file("1.zip")
 
 excel = Spreadsheet.open('1.xls').worksheet 0
+a=1
+b=1
+#c=1
+i=1
 code=0
 item=1
 price=2
 subgroup=''
-yamlfile = File.open('1.yml', 'w')
+yamlfile = File.open('1.yaml', 'w')
   excel.each do |rows|
-    if rows.default_format and rows[code].to_i==0
-        yamlfile << rows[item].to_s << "\n" if rows.default_format.pattern_fg_color==:border and rows.idx>9
+    if rows.default_format and rows[code].to_i==0 and rows.idx>9
+	  if rows.default_format.pattern_fg_color==:border
+		yamlfile << a << "group:"<< "\n"<< "  "<< "group: '"<< rows[item].to_s.gsub("'"," ") << "'\n"
+		a+=1
+	  end
       if rows.default_format.pattern_fg_color==:white
         subgroup=rows[item]
-        yamlfile << "  " << subgroup.to_s << "\n"
+        yamlfile << "  " << b << "subgroup:"<< "\n"<< "    "<< "subgroup: '"<< subgroup.to_s.gsub("'"," ") << "'\n"
+        b+=1
       else
         subgroup=''
       end
     end 
-    if rows.default_format and rows.default_format.pattern_fg_color==:border and rows[code].to_i!=0
-      yamlfile << "  " << rows[item].to_s << "\n" << "  " << rows[code] << "\n" << "  " << rows[price]<< "\n" if subgroup==''
-      yamlfile << "    " << rows[item].to_s << "\n" << "    " << rows[code] << "\n" << "    " << rows[price]<< "\n" unless subgroup==''
-    end
+    if rows.default_format and rows.default_format.pattern_fg_color==:border and rows[code].to_i!=0 
+      if subgroup==''
+        yamlfile << "  " << i << "product:" << "\n"<< "    "<< "name: '"<< rows[item].gsub("'"," ") << "'\n" << "    " << "code: '" << rows[code] << "'\n" << "    " << "price: '" << rows[price]<< "'\n"
+        i+=1
+      end
+      unless subgroup==''
+		yamlfile << "    " << i << "product:" << "\n"<< "      "<< "name: '"<< rows[item].gsub("'"," ") << "'\n" << "      " << "code: '" << rows[code] << "'\n" << "      " << "price: '" << rows[price]<< "'\n"
+		i+=1
+      end
+    end   
   end
   FileUtils.remove_file("1.xls")
